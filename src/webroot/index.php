29ba80c2a427c0b789a89c7ee5c64a226f652102
Jaime Trigo <hegeltrigo@hotmail.com>
2012-04-18 13:56:57 -0400

    vhost config

--- src/webroot/index.php
+++ src/webroot/index.php
@@ -58,7 +58,7 @@ if (!defined('APP_DIR')) {
  *
  * Leaving this constant undefined will result in it being defined in Cake/bootstrap.php
  */
-define('CAKE_CORE_INCLUDE_PATH', 'C:' . DS . 'xampp' . DS . 'cakephp-2.0' . DS . 'lib');
+define('CAKE_CORE_INCLUDE_PATH', 'C:' . DS . 'xampp' . DS . 'cakephp-2.1' . DS . 'lib');
 
 /**
  * Editing below this line should NOT be necessary.
